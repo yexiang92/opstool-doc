10302f6780b6f59c38366f54f35975c58e58f124
Yexiang Yan <yexiang.yan@mail.mcgill.ca>
2025-11-09 21:50:11 -0500

    add to readthedocs

---
+++ conf.py
@@ -0,0 +1,10 @@
+# docs/conf.py
+project = "opstool"
+extensions = []
+templates_path = []
+exclude_patterns = []
+
+html_theme = "alabaster"  # No
+html_static_path = []
+
+html_extra_path = ["html"]
